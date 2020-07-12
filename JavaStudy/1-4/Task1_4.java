bbfd24193a0afd276acef9444cb5c199a4e5e090
kazuya989 <66933018+kazuya989@users.noreply.github.com>
2020-07-13 03:44:24 +0900

    制御分(条件分岐)
    制御文（条件分岐）[if, else, else if, switch]の使い方

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -31,19 +31,20 @@ public class Task1_4 {
          String name = "alice";
          String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、「 ログイン成功です 」と出力してください。
-        if(CONST_ALICE.equals(name)  && CONST_PASS.equals(pass)){
+        if(CONST_ALICE.equals(name) && (CONST_PASS.equals(pass))){
             System.out.println(CONST_MSG_SUCCESS);
         }
+        
 
 
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        else if(CONST_ALICE.equals(name) && CONST_PASS != pass){
+        else if((CONST_ALICE.equals(name)) && !(CONST_PASS.equals(pass))){
             System.out.println(CONST_MSG_ERROR_PASS);
         }
 
 
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        else if(CONST_ALICE != name && CONST_PASS.equals(pass)){
+        else if(!(CONST_ALICE.equals(name)) && (CONST_PASS.equals(pass))){
             System.out.println(CONST_MSG_ERROR_PASS);
         }
 
